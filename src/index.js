d61ebbc90af05393004ffb02b31f70ba03d0711b
tanguyrousseau1 <61149353+tanguyrousseau1@users.noreply.github.com>
2023-01-12 19:40:02 +0100

    added update and delete endpoint for news and user

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const {startDatabase} = require('./database/mongo');
-const {insertNews, getNews} = require('./database/news');
-const {insertUser, getUser} = require('./database/user');
+const {insertNews, getNews, deleteNews, updateNews} = require('./database/news');
+const {insertUser, getUser, deleteUser, updateUser} = require('./database/user');
 
 const app = express();
 
@@ -17,7 +17,20 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(morgan('combined'));
 
-//enpoint
+//endpoints
+//post
+app.post('/news', async (req,res) => {
+    const newNews = req.body;
+    await insertNews(newNews);
+    res.send({ message: 'New news inserted.' });
+});
+app.post('/user', async(req,res) => {
+    const newUser = req.body;
+    await insertUser(newUser);
+    res.send({ message: "New user inserted." });
+});
+
+//get
 app.get('/', (req, res) => {
     res.send("this is the root endpoint")
 });
@@ -26,6 +39,28 @@ app.get('/news', async (req,res) => {
 });
 app.get('/user', async (req,res) => {
     res.send(await getUser());
+});
+
+//delete
+app.delete('/news:id', async (req,res) => {
+    await deleteNews(req.params.id);
+    res.send({ message: "News removed." });
+});
+app.delete('/user:id', async (req,res) => {
+    await deleteUser(req.params.id);
+    res.send({ message: "User removed." });
+});
+
+//update
+app.put('/news:id', async (req,res) => {
+    const updatedNews = req.body;
+    await updateNews(req.params.id, updatedNews);
+    res.send({ message: "News updated." });
+});
+app.put('/user:id', async (req,res) => {
+    const updatedUser = req.body;
+    await updateUser(req.params.id, updatedUser);
+    res.send({ message: "User updated." });
 });
 
 //start in-memory mongoDB instance
